f2ba9a99b8f84c5cbffcac63dfe1f5517b61052f
Jake Bloom <jake@steady.health>
2019-09-26 18:07:22 -0700

    Initial Commit

---
+++ WatchYaself WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchYaself WatchKit Extension
+//
+//  Created by Jake Bloom on 26/9/19.
+//  Copyright © 2019 Jake Bloom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchYaself WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchYaself WatchKit Extension
+//
+//  Created by Jake Bloom on 26/9/19.
+//  Copyright © 2019 Jake Bloom. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
